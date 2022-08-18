95b6d4b260abc546c1cc8e5f01f39a75a54ea482
naomitheworld <94208136+naomitheworld@users.noreply.github.com>
2022-08-18 09:47:44 -0600

    Add files via upload

---
+++ EVA1-2-STACKOVERFLOW/src/eva1/pkg2/stackoverflow/EVA12STACKOVERFLOW.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1.pkg2.stackoverflow;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA12STACKOVERFLOW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        A();
+    }
+}
