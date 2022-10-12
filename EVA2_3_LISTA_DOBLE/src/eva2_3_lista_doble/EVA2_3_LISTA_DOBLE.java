20206383295606a18f8530978488698615bd0e95
naomitheworld <94208136+naomitheworld@users.noreply.github.com>
2022-10-12 09:23:55 -0600

    Add files via upload

---
+++ EVA2_3_LISTA_DOBLE/src/eva2_3_lista_doble/EVA2_3_LISTA_DOBLE.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_3_lista_doble;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA2_3_LISTA_DOBLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ListaDoble listaDoble = new ListaDoble();
+        listaDoble.add(10);
+        listaDoble.add(10);
+        listaDoble.add(10);
+        listaDoble.add(10);
+        listaDoble.add(10);
+    }
+    
+}
---
+++ EVA2_3_LISTA_DOBLE/src/eva2_3_lista_doble/ListaDoble.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_3_lista_doble;
+
+/**
+ *
+ * @author moviles
+ */
+public class ListaDoble {
+
+    private Nodo inicio;
+    private Nodo fin;
+    private int c;
+
+
+    public ListaDoble() {
+        this.inicio = null; // no hay nodos en la lista
+        this.fin = null;
+        this.c = 0;
+    }
+
+    public void imprimir() {
+        Nodo temp = inicio;
+// como muevo a temp?
+        if (inicio == null) {
+            System.err.println("Lista vacia");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.getValor() + " - ");
+                temp = temp.getSiguiente();
+            }
+        }
+        System.out.println("");
+    }
+
+    // agregar un nodo al final de la lista
+    // solucion 1: O(n^2)
+    public void add(int valor) {
+        Nodo nvoNodo = new Nodo(valor);
+        // verificar si hay nodos en la lista
+        if (inicio == null) { // no hay nodos en la lista
+            inicio = nvoNodo;
+            fin = nvoNodo;
+        } else { // hay nodos en la lista
+            // hay que movernos por la lista 
+            // usando un nodo temporal hasta el ultimo nodo de la lista
+            /*Nodo temp = inicio;
+             while (temp.getSiguiente() != null) {
+             temp = temp.getSiguiente();
+             }
+             temp.setSiguiente(nvoNodo);*/
+            fin.setSiguiente(nvoNodo);
+            nvoNodo.setPrevio(fin);
+            fin = nvoNodo;
+        }
+        c++;
+    }
+
+    public int size() {
+        /*int c = 0;
+        Nodo temp = inicio;
+        while (temp != null) {
+            c++;
+            temp = temp.getSiguiente();
+        }*/
+        return this.c;
+    }
+
+    public void addIn(int valor, int p) throws Exception {
+        int t = size();
+        /*que debemos validar?
+         insertar en una posicion no valida
+         posiciones negativas
+         posiciones mayores a la cantidad de elementos
+         */
+        if (p < 0) {  // si es negativo, lanza un error
+            throw new Exception("No puede insertarse un nodo en una posición negativa");
+        } else if (p >= t) { //posiciones mayores a la cantidad de elementos
+            throw new Exception(p + " no es una posición válida en la lista");
+        } else {
+            Nodo nvoNodo = new Nodo(valor);
+            if (p == 0) {
+                nvoNodo.setSiguiente(inicio);
+                inicio = nvoNodo;
+            } else {
+                Nodo temp = inicio;
+                int c = 0;
+                while (c <= (p - 1)) {
+                    temp = temp.getSiguiente();
+                    c++;
+                }
+                nvoNodo.setSiguiente(temp.getSiguiente());
+                temp.setSiguiente(nvoNodo);
+            }
+        }
+        this.c++;
+    }
+
+    public void empty() {
+        inicio = null;
+        fin = null;
+        c = 0;
+    }
+
+    // if we delete the last node, we need to move the end node
+    // if the list has only one node its best we call empty list
+    // if its the first node we only change the start node
+    // if there are no nodes in the list, nothing is deleted
+    public void deleteIn(int p) throws Exception {
+        int t = size();
+        /*que debemos validar?
+         borrar en una posicion no valida
+         posiciones negativas
+         posiciones mayores a la cantidad de elementos
+         */
+        if (p < 0) {  // si es negativo, lanza un error
+            throw new Exception("No puede insertarse un nodo en una posición negativa");
+        } else if (p > t) { //posiciones mayores a la cantidad de elementos
+            throw new Exception(p + " no es una posición válida en la lista");
+        } else {
+            // piensen como programadores:
+            // que puede salir mal si:
+            if (t == 1) {
+                empty();
+            } else {
+                // borrar el primer nodo
+                // borrar nodo intermedio
+                // borrar el ultimo nodo
+                if (p == 0) {
+                    inicio = inicio.getSiguiente();
+                } else {
+                    Nodo temp = inicio;
+                    int c = 0;
+                    while (c <= (p - 1)) {
+                        temp = temp.getSiguiente();
+                        c++;
+                    }
+                    // ya estoy en el nodo previo
+                    Nodo next_obj = temp.getSiguiente();
+                    temp.setSiguiente(next_obj.getSiguiente());
+                    if (p == (t - 1)) {
+                        fin = temp;
+                    }
+                }
+                this.c--;
+            }
+        }
+    }
+
+    public int getValueAt(int p, int valor) throws Exception {
+        int t = size();
+        if (p < 0) {  // si es negativo, lanza un error
+            throw new Exception("No puede insertarse un nodo en una posición negativa");
+        } else if (p > t) { //posiciones mayores a la cantidad de elementos
+            throw new Exception(p + " no es una posición válida en la lista");
+        } else {
+            Nodo temp = inicio;
+            int c = 0;
+            while (c < p) {
+                temp = temp.getSiguiente();
+                c++;
+            }
+            // ya estoy en el nodo previo
+            valor = temp.getValor();
+        }
+        return valor;
+    }
+}
