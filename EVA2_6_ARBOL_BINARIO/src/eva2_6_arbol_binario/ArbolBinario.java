4478c5d927440a6068f71138455c4f69e1403ad2
naomitheworld <94208136+naomitheworld@users.noreply.github.com>
2022-10-21 09:54:18 -0600

    Add files via upload

---
+++ EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/ArbolBinario.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_6_arbol_binario;
+
+/**
+ *
+ * @author moviles
+ */
+public class ArbolBinario {
+
+    private Nodo root;
+
+    public ArbolBinario(Nodo root) {
+        this.root = null;
+    }
+
+    // metodo publico para el usuario
+    public void add(int valor) {
+        add_recursive(valor, root);
+    }
+
+    //metodo recursivo agregar
+    // ncesita el valor y el nodo igual
+    private void add_recursive(int valor, Nodo c_node) {
+        if (true) {
+
+        } else {
+           
+        }
+    }
+}
---
+++ EVA2_6_ARBOL_BINARIO/src/eva2_6_arbol_binario/EVA2_6_ARBOL_BINARIO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_6_arbol_binario;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA2_6_ARBOL_BINARIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
